d76e1bde4a467466a3aac744c8919a4fd87cbb6a
Lena <leshshsh@gmail.com>
2020-10-30 23:58:22 +0300

    Первый коммент

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
-'use strick';
+'use strict';
 
-const category ='toys';
 
-console.log(`https://someurl.com/${category}/5`);
 
-const user = 'Ivan';
+let incr = 10,
+    decr = 10;
 
-alert('Привет, user');
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2 + 2 * 2 !== 6);
+
+const isChecked = false;
+      isClose = false;
+
+console.log(isChecked || !isClose);
+
